c4e7e636ebe6ed4f686d106f685b75171057e861
Peter <warrenp11@gmail.com>
2022-01-31 11:40:16 -0500

    query to return all thoughts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,18 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+  }
+
   type Query {
-    helloWorld: String
+    thoughts: [Thought]
   }
 `;
 
