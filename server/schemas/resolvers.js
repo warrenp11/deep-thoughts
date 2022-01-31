8fde642c3063744921b36f42f5e0d571fb686357
Peter <warrenp11@gmail.com>
2022-01-31 13:29:50 -0500

    Create the Remaining Query Resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,30 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 const resolvers = {
   Query: {
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // get thoughts by username
     thoughts: async (parent, { username }) => {
-        const params = username ? { username } : {};
-        return Thought.find(params).sort({ createdAt: -1 });
-      },
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    // get single thought by _id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
   },
 };
 
